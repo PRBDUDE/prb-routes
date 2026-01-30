64d728cca595658e32ae1d9db65b4b36e2059835
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-30 15:44:11 -0500

    clean up code

--- src/app/feature/top-level-2/side-menu/side-menu.ts
+++ src/app/feature/top-level-2/side-menu/side-menu.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'prb-side-menu',
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './side-menu.html',
   styleUrl: './side-menu.scss',
 })
