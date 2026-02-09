3aa646ce992e12dd1a6b4d9be679e664bcc52b41
PRBDUDE <programmerroadblocks@gmail.com>
2026-02-09 10:24:18 -0500

    add sub-router-outlet routes

---
+++ src/app/feature/sub-router-outlet/sub-router-outlet-routes.ts
@@ -0,0 +1,55 @@
+import {Routes} from '@angular/router';
+
+export const subRouterOutletRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./sub-router-outlet')
+      .then(m => m.SubRouterOutlet),
+    title: 'Sub Router Outlet Base',
+    children: [
+      {
+        path: 'sub-path-1',
+        loadComponent: () =>
+          import('./sub-path-1/sub-path-1')
+          .then(m => m.SubPath1),
+        outlet: 'left-panel'
+      },
+      {
+        path: 'sub-path-1-1',
+        loadComponent: () =>
+          import('./sub-path-1-1/sub-path-1-1')
+          .then(m => m.SubPath11),
+        outlet: 'left-panel'
+      },
+      {
+        path: 'sub-path-1-2',
+        loadComponent: () =>
+          import('./sub-path-1-2/sub-path-1-2')
+          .then(m => m.SubPath12),
+        outlet: 'lower-left-panel'
+      },
+      {
+        path: 'sub-path-1-3',
+        loadComponent: () =>
+          import('./sub-path-1-3/sub-path-1-3')
+          .then(m => m.SubPath13),
+        outlet: 'lower-left-panel'
+      },
+      {
+        path: 'sub-path-2',
+        loadComponent: () =>
+          import('./right-sub-path-2/right-sub-path-2')
+          .then(m => m.RightSubPath2),
+        outlet: 'right-panel'
+      },
+      {
+        path: 'sub-path-2-1',
+        loadComponent: () =>
+          import('./right-sub-path-2-1/right-sub-path-2-1')
+          .then(m => m.RightSubPath21),
+        outlet: 'right-panel'
+      }
+    ]
+  }
+];
